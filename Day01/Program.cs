a10ca965ec4ed46b6a9d9416505ace04e078625f
Mikko Heikelä <mikko.heikela@gmail.com>
2019-12-01 05:13:35 +0000

    Add tidied up solution to day 1

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Day01
 {
     class Program
     {
+        static int FuelForMass(int mass)
+        {
+            return Math.Max(0, mass / 3 - 2);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IEnumerable<string> lines = File.ReadLines("../../../input.txt");
+
+            int fuel1 = 0;
+            int fuel2 = 0;
+            foreach (string line in lines)
+            {
+                int mass = int.Parse(line);
+                fuel1 += FuelForMass(mass);
+                while (FuelForMass(mass) > 0)
+                {
+                    mass = FuelForMass(mass);
+                    fuel2 += mass;
+                }
+            }
+
+            Console.WriteLine($"Part1 = {fuel1}");
+            Console.WriteLine($"Part2 = {fuel2}");
         }
     }
 }
