4fe552755f92760d2030c41bd8384bfa15c38c6e
Mikko Heikelä <mikko.heikela@gmail.com>
2019-12-02 05:50:40 +0000

    Add tidied up version of day 2

---
+++ Day02/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day02
+{
+    class Program
+    {
+        static void ExecuteBinOp(List<int> values, int pc, Func<int, int, int> op)
+        {
+            int srcAddr1 = values[pc + 1];
+            int srcAddr2 = values[pc + 2];
+            int destAddr = values[pc + 3];
+            values[destAddr] = op(values[srcAddr1], values[srcAddr2]);
+        }
+
+        static int RunIntCode(string input, int noun, int verb)
+        {
+            List<int> values = input.Split(',').Select(line => int.Parse(line)).ToList();
+
+            values[1] = noun;
+            values[2] = verb;
+
+            int pc = 0;
+            while (true)
+            {
+                int opcode = values[pc];
+                switch (values[pc])
+                {
+                    case 1:
+                        {
+                            ExecuteBinOp(values, pc, (a, b) => a + b);
+                            pc += 4;
+                            break;
+                        }
+                    case 2:
+                        {
+                            ExecuteBinOp(values, pc, (a, b) => a * b);
+                            pc += 4;
+                            break;
+                        }
+                    case 99:
+                        return values[0];
+                    default:
+                        throw new Exception($"ERROR: unknown Opcode {opcode} at address {pc}");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string input = File.ReadLines("../../../input.txt").First();
+            //string input = "1,9,10,3,2,3,11,0,99,30,40,50";
+            //string input = "1,0,0,0,99";
+
+            Console.WriteLine($"Part1 = {RunIntCode(input, 12, 2)}");
+            for (int noun = 0; noun < 100; ++noun)
+            {
+                for (int verb = 0; verb < 100; ++verb)
+                {
+                    int result = RunIntCode(input, noun, verb);
+                    if (result == 19690720)
+                    {
+                        Console.WriteLine($"Part2 = {100 * noun + verb}");
+                    }
+                }
+            }
+        }
+    }
+}
