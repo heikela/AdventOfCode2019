1026e0b0ab6c14b9ee47093b29a8a2e50bbee585
Mikko Heikelä <mikko.heikela@gmail.com>
2019-12-09 19:45:48 +0000

    Fix warnings about missing GetHashCode in IEquatable IntPoint structs

--- Common/Point.cs
+++ Common/Point.cs
@@ -11,10 +11,12 @@ namespace Common
 			X = x;
 			Y = y;
 		}
+
 		public static IntPoint2D operator +(IntPoint2D a, IntPoint2D b)
 		{
 			return new IntPoint2D(a.X + b.X, a.Y + b.Y);
 		}
+
 		public static IntPoint2D operator -(IntPoint2D a, IntPoint2D b)
 		{
 			return new IntPoint2D(a.X - b.X, a.Y - b.Y);
@@ -29,10 +31,26 @@ namespace Common
 		{
 			return X == p.X && Y == p.Y;
 		}
-		public static bool operator ==(IntPoint2D a, IntPoint2D b)
+
+        public override bool Equals(object obj)
+        {
+            if (obj is IntPoint2D)
+            {
+                return this.Equals((IntPoint2D)obj);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+        public static bool operator ==(IntPoint2D a, IntPoint2D b)
 		{
 			return a.Equals(b);
 		}
+
 		public static bool operator !=(IntPoint2D a, IntPoint2D b)
 		{
 			return !(a.Equals(b));
@@ -42,6 +60,7 @@ namespace Common
 		{
 			return Math.Abs(X) + Math.Abs(Y);
 		}
+
 		public int ManhattanDist(IntPoint2D other)
 		{
 			return (this - other).ManhattanDist();
@@ -59,10 +78,12 @@ namespace Common
 			Y = y;
 			Z = z;
 		}
+
 		public static IntPoint3D operator +(IntPoint3D a, IntPoint3D b)
 		{
 			return new IntPoint3D(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
 		}
+
 		public static IntPoint3D operator -(IntPoint3D a, IntPoint3D b)
 		{
 			return new IntPoint3D(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
@@ -81,19 +102,28 @@ namespace Common
 			}
 			return false;
 		}
+
 		public bool Equals(IntPoint3D p)
 		{
 			return X == p.X && Y == p.Y && Z == p.Z;
 		}
+
 		public static bool operator ==(IntPoint3D a, IntPoint3D b)
 		{
 			return a.Equals(b);
 		}
+
 		public static bool operator !=(IntPoint3D a, IntPoint3D b)
 		{
 			return !(a.Equals(b));
 		}
-		public int ManhattanDist()
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y, Z);
+        }
+
+        public int ManhattanDist()
 		{
 			long d = (long)Math.Abs(X) + (long)Math.Abs(Y) + (long)Math.Abs(Z);
 			if (d > int.MaxValue)
@@ -102,6 +132,7 @@ namespace Common
 			}
 			return (int)d;
 		}
+
 		public int ManhattanDist(IntPoint3D other)
 		{
 			return (this - other).ManhattanDist();
