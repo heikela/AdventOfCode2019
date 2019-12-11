19892889d73dea718ad598219678aafb34d6ca5a
Mikko Heikelä <mikko.heikela@gmail.com>
2019-12-11 07:35:28 +0000

    Permutations that should cope with duplicate or incomparable elements
    Based on permutations of a range of integers

--- Common/Permutations.cs
+++ Common/Permutations.cs
@@ -6,21 +6,34 @@ namespace Common
 {
     public static class Permutation
     {
-        public static List<List<T>> Permutations<T>(List<T> items) where T : IEquatable<T>
+        public static List<List<int>> PermutationsOfRange(int n)
+        {
+            List<int> items = Enumerable.Range(0, n).ToList();
+            return PermutationsOfIntsWithoutDuplicates(items);
+        }
+
+        public static List<List<T>> Permutations<T>(List<T> items)
+        {
+            return PermutationsOfRange(items.Count)
+                .Select(perm => perm.Select(i => items[i]).ToList())
+                .ToList();
+        }
+
+        private static List<List<int>> PermutationsOfIntsWithoutDuplicates(List<int> items)
         {
             if (items.Count == 0)
             {
-                return new List<List<T>>() { new List<T>() };
+                return new List<List<int>>() { new List<int>() };
             }
             else if (items.Count == 1)
             {
-                return new List<List<T>>() { new List<T>() { items[0] } };
+                return new List<List<int>>() { new List<int>() { items[0] } };
             }
             return items.SelectMany(item => {
-                var permutationsOfTheRest = Permutations(items.Where(remainingItem => !remainingItem.Equals(item)).ToList());
+                var permutationsOfTheRest = PermutationsOfIntsWithoutDuplicates(items.Where(remainingItem => remainingItem != item).ToList());
                 return permutationsOfTheRest
-                    .Select<List<T>, List<T>>(
-                        permutationOfTheRest => permutationOfTheRest.Concat(new List<T>() { item }).ToList())
+                    .Select(
+                        permutationOfTheRest => permutationOfTheRest.Concat(new List<int>() { item }).ToList())
                     .ToList();
             }).ToList();
         }
