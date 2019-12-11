e80cba27dc2bf53f0e05033cdc82136bbb812da1
Mikko Heikelä <mikko.heikela@gmail.com>
2019-12-11 06:45:17 +0000

    Day 11 solution

--- Common/SparseGrid.cs
+++ Common/SparseGrid.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.IO;
+using System;
 
 namespace Common
 {
@@ -31,6 +32,30 @@ namespace Common
         public static Dictionary<IntPoint2D, char> ReadFromFile(string filename)
         {
             return Read(File.ReadLines(filename));
+        }
+
+        public static void Print<T>(Dictionary<IntPoint2D, T> grid, Func<T, char> elemPrinter)
+        {
+            int minY = grid.Keys.Min(p => p.Y);
+            int maxY = grid.Keys.Max(p => p.Y);
+            int minX = grid.Keys.Min(p => p.X);
+            int maxX = grid.Keys.Max(p => p.X);
+            for (int y = minY; y <= maxY; ++y)
+            {
+                for (int x = minX; x <= maxX; ++x)
+                {
+                    IntPoint2D pos = new IntPoint2D(x, y);
+                    if (grid.ContainsKey(pos))
+                    {
+                        Console.Write(elemPrinter(grid[pos]));
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.IO;
+using Common;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
 
 namespace Day11
 {
     class Program
     {
+        static Dictionary<IntPoint2D, int> RunPaintRobot(string program, Dictionary<IntPoint2D, int> hullAtStart)
+        {
+            IntCodeComputer computer = new IntCodeComputer(program);
+            Dictionary<IntPoint2D, int> hull = new Dictionary<IntPoint2D, int>(hullAtStart);
+            Queue<BigInteger> inputs = new Queue<BigInteger>();
+            IntPoint2D position = new IntPoint2D();
+            List<IntPoint2D> directions = new List<IntPoint2D>()
+            {
+                new IntPoint2D(0, -1),
+                new IntPoint2D(1, 0),
+                new IntPoint2D(0, 1),
+                new IntPoint2D(-1, 0)
+            };
+            int directionIndex = 0;
+            (bool running, IEnumerable<BigInteger> output) result;
+            do
+            {
+                inputs.Enqueue(hull.GetOrElse(position, 0));
+                result = computer.RunIntCode(inputs);
+                if (result.output.Count() != 2)
+                {
+                    throw new Exception("Expected a painting and a movement command");
+                }
+                hull.AddOrSet(position, (int)result.output.ElementAt(0));
+                BigInteger turn = result.output.ElementAt(1);
+                if (turn == 0)
+                {
+                    --directionIndex;
+                }
+                else if (turn == 1)
+                {
+                    ++directionIndex;
+                }
+                directionIndex = (directionIndex + directions.Count) % directions.Count;
+                position += directions[directionIndex];
+            } while (result.running);
+
+            return hull;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string program = File.ReadLines("../../../input.txt").First();
+            Dictionary<IntPoint2D, int> hull = RunPaintRobot(program, new Dictionary<IntPoint2D, int>());
+
+            Console.WriteLine($"The robot painted {hull.Count} positions");
+
+            hull = RunPaintRobot(program, new Dictionary<IntPoint2D, int>() { { new IntPoint2D(), 1 } });
+
+            SparseGrid.Print(hull, i => i == 1 ? '#' : ' ');
         }
     }
 }
