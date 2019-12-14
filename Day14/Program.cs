725c694018ebadfab45e50971ccab92b09002eb0
Mikko Heikelä <mikko.heikela@gmail.com>
2019-12-14 08:31:50 +0000

    Factor out a NanoFactory object. Not sure if improvement.

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -7,73 +7,37 @@ using Common;
 
 namespace Day14
 {
-    struct Ingredient {
-        public int Quantity;
-        public string Chemical;
-    }
-
-    class Conversion
+    class NanoFactory
     {
-        public int QuantityProduced;
-        public string ChemicalProduced;
-        public List<Ingredient> Ingredients;
-    }
-
-    class Program
-    {
-        static Dictionary<string, long> Surpluses = new Dictionary<string, long>();
-        static long OreSpent;
-        static Dictionary<string, Conversion> Conversions = new Dictionary<string, Conversion>();
-
-        static void UseSurplusOrMake(string chemical, long quantity)
+        struct Ingredient
         {
-            long availableSurplus = Surpluses.GetOrElse(chemical, 0);
-            if (availableSurplus > 0)
-            {
-                long surplusUsed = Math.Min(quantity, availableSurplus);
-                quantity -= surplusUsed;
-                Surpluses[chemical] -= surplusUsed;
-            }
-            if (chemical == "ORE")
-            {
-                OreSpent += quantity;
-                return;
-            }
-            if (quantity > 0)
-            {
-                Make(chemical, quantity);
-            }
+            public int Quantity;
+            public string Chemical;
         }
 
-        static void Make(string chemical, long quantity)
+        class Conversion
         {
-            Conversion conversion = Conversions[chemical];
-
-            long multiplier = (quantity - 1 + conversion.QuantityProduced) / conversion.QuantityProduced;
-            long producedQuantity = multiplier * conversion.QuantityProduced;
-            long surplusProduced = producedQuantity - quantity;
-            foreach (Ingredient i in conversion.Ingredients)
-            {
-                UseSurplusOrMake(i.Chemical, i.Quantity * multiplier);
-            }
-            if (surplusProduced > 0)
-            {
-                if (Surpluses.ContainsKey(chemical))
-                {
-                    Surpluses[chemical] += surplusProduced;
-                } else
-                {
-                    Surpluses.Add(chemical, surplusProduced);
-                }
-            }
+            public int QuantityProduced;
+            public string ChemicalProduced;
+            public List<Ingredient> Ingredients;
         }
 
-        static void Main(string[] args)
+        private Dictionary<string, Conversion> Conversions = new Dictionary<string, Conversion>();
+
+        private Dictionary<string, long> Surpluses = new Dictionary<string, long>();
+        private long OreSpent;
+
+        private void Reset()
+        {
+            Surpluses = new Dictionary<string, long>();
+            OreSpent = 0;
+        }
+
+        public NanoFactory(IEnumerable<string> conversionFormulas)
         {
             Regex reactionPattern = new Regex(@"(.*?) => (\d+) (\w+)");
             Regex ingredientPattern = new Regex(@"(\d+) (\w+)");
-            Conversions = File
-                .ReadLines("../../../input.txt")
+            Conversions = conversionFormulas
                 .Select(line =>
                 {
                     Match leftAndRight = reactionPattern.Match(line);
@@ -101,19 +65,84 @@ namespace Day14
 
                 })
                 .ToDictionary();
-            Make("FUEL", 1);
-            Console.WriteLine($"1 FUEL can be produced from {OreSpent} ORE");
-            long fuelProduced = 1;
+            Reset();
+        }
+
+        public long MakeFromOre(string chemical, long quantity)
+        {
+            Reset();
+            Make(chemical, quantity);
+            return OreSpent;
+        }
+
+        public long HowManyCanBeMadeFrom(string chemical, long oreQuantity)
+        {
+            Reset();
+            Make(chemical, 1);
             long maxOreNeeded = OreSpent;
-            long trillion = 1000000000000;
-            while (OreSpent < trillion)
+            long produced = 1;
+            while (OreSpent < oreQuantity)
             {
-                long fuelLowerBound = Math.Max(1, (trillion - OreSpent) / maxOreNeeded);
-                Make("FUEL", fuelLowerBound);
-                fuelProduced += fuelLowerBound;
+                long productionLowerBound = Math.Max(1, (oreQuantity - OreSpent) / maxOreNeeded);
+                Make(chemical, productionLowerBound);
+                produced += productionLowerBound;
             }
-            Console.WriteLine($"{fuelProduced} FUEL can be produced from {OreSpent} ORE");
-            Console.WriteLine($"Therefore {fuelProduced - 1} should be the answer");
+            return produced - 1;
+        }
+
+        private void UseSurplusOrMake(string chemical, long quantity)
+        {
+            long availableSurplus = Surpluses.GetOrElse(chemical, 0);
+            if (availableSurplus > 0)
+            {
+                long surplusUsed = Math.Min(quantity, availableSurplus);
+                quantity -= surplusUsed;
+                Surpluses[chemical] -= surplusUsed;
+            }
+            if (chemical == "ORE")
+            {
+                OreSpent += quantity;
+                return;
+            }
+            if (quantity > 0)
+            {
+                Make(chemical, quantity);
+            }
+        }
+
+        private void Make(string chemical, long quantity)
+        {
+            Conversion conversion = Conversions[chemical];
+
+            long multiplier = (quantity - 1 + conversion.QuantityProduced) / conversion.QuantityProduced;
+            long producedQuantity = multiplier * conversion.QuantityProduced;
+            long surplusProduced = producedQuantity - quantity;
+            foreach (Ingredient i in conversion.Ingredients)
+            {
+                UseSurplusOrMake(i.Chemical, i.Quantity * multiplier);
+            }
+            if (surplusProduced > 0)
+            {
+                if (Surpluses.ContainsKey(chemical))
+                {
+                    Surpluses[chemical] += surplusProduced;
+                }
+                else
+                {
+                    Surpluses.Add(chemical, surplusProduced);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NanoFactory factory = new NanoFactory(File.ReadLines("../../../input.txt"));
+            Console.WriteLine($"1 FUEL can be produced from {factory.MakeFromOre("FUEL", 1)} ORE");
+            long trillion = 1000000000000;
+            Console.WriteLine($"{factory.HowManyCanBeMadeFrom("FUEL", trillion)} should be the answer to how much fuel can be made from {trillion} ore");
         }
     }
 }
