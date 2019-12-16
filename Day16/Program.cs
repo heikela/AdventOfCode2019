04dfb35fb4d3f5f250b96359b4cf4e2b5756efb2
Mikko Heikelä <mikko.heikela@gmail.com>
2019-12-16 07:01:55 +0000

    Solve Day 16 Part 2 with partial sums

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -9,21 +9,15 @@ namespace Day16
 {
     class Program
     {
-        static List<int> BasePattern = new List<int>
+        public static IEnumerable<TResult> Scan<TResult, T>(IEnumerable<T> source, TResult initial, Func<TResult, T, TResult> aggregator)
         {
-            0, 1, 0, -1
-        };
-
-        static int convolution(IEnumerable<int> first, IEnumerable<int> second)
-        {
-            var a = first.GetEnumerator();
-            var b = second.GetEnumerator();
-            int sum = 0;
-            while (a.MoveNext() && b.MoveNext())
+            TResult currentResult = initial;
+            foreach (T element in source)
             {
-                sum += a.Current * b.Current;
+                currentResult = aggregator(currentResult, element);
+                yield return currentResult;
             }
-            return sum;
+            yield break;
         }
 
         static int multiplier(int outpos, int inpos)
@@ -39,52 +33,57 @@ namespace Day16
             }
         }
 
-        static int FFTconvolution(IEnumerable<int> first, int offset)
-        {        
-            var a = first.GetEnumerator();
-            int sum = 0;
-            int i = offset;
-            while (a.MoveNext())
+        static int FFTconvolution(List<int> partialSums, int pos)
+        {
+            int repeatLength = (pos + 1);
+            int start = repeatLength - 1;
+            int sign = 1;
+            long sum = 0;
+            while (start < partialSums.Count)
             {
-                sum += a.Current * multiplier(offset, i);
-                ++i;
+                int end = Math.Min(start + repeatLength, partialSums.Count - 1);
+                sum += sign * (partialSums[end] - partialSums[start]);
+                start += 2 * repeatLength;
+                sign *= -1;
             }
-            return sum;
+            return (int)sum;
         }
 
-        static IEnumerable<int> FFT(IEnumerable<int> input, int offset = 0)
+        static void CalculateFFTElement(List<int> partialSums, List<int> current, int pos)
         {
-            var inputEnumerator = input.GetEnumerator();
-            int i = 0;
-            while (inputEnumerator.MoveNext())
-            {
-                var repeatingPattern = BasePattern.SelectMany(n => Enumerable.Repeat(n, i + offset + 1));
-                var repeatedPattern = Enumerable.Repeat(repeatingPattern, 100000).Flatten();
-                int sum = convolution(input.Skip(i), repeatedPattern.Skip(i + offset + 1));
-                yield return Math.Abs(sum) % 10;
-                ++i;
-            }
-            yield break;
+            current[pos] = Math.Abs(FFTconvolution(partialSums, pos)) % 10;
         }
 
         static void Main(string[] args)
         {
             string input = File.ReadLines("../../../input.txt").First();
             int skip = 5971981;
-            //string input = "12345678";
-            List<int> values = Enumerable.Repeat(input.AsEnumerable().Select(c => int.Parse($"{c}")), 10000).Flatten().ToList();
+//            int skip = 293510;
+            //string input = "02935109699940807407585447034323";
+            int repeats = 10000;
+            List<int> values = Enumerable.Repeat(input.AsEnumerable().Select(c => int.Parse($"{c}")), repeats).Flatten().ToList();
 
+            List<int> prev = values;
+            List<int> current = values.Select(i => 0).ToList();
             for (int i = 0; i < 100; ++i)
             {
-                values = FFT(values, skip).ToList();
-                Console.WriteLine($"Phase {i + 1}:");
-                foreach (var n in values.Take(8))
+                List<int> partialSums = Scan(prev, 0, (a, b) => a + b).ToList();
+                partialSums.Insert(0, 0);
+                for (int pos = 0; pos < current.Count; ++pos)
                 {
-                    Console.Write(n);
+                    CalculateFFTElement(partialSums, current, pos);
                 }
-                Console.WriteLine("");
+                prev = current;
+                current = current.Select(i => 0).ToList();
+
+                Console.WriteLine($"Phase {i + 1} calculated");
             }
 
+            for (int pos = skip; pos < skip + 8; ++pos)
+            {
+                Console.Write(prev[pos]);
+            }
+            Console.WriteLine("");
             // not 020619550
         }
     }
