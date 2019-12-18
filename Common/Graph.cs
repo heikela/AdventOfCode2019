f42856c25139fe9621996bc78137bf4dc93f6927
Mikko Heikelä <mikko.heikela@gmail.com>
2019-12-18 08:49:12 +0000

    WIP

--- Common/Graph.cs
+++ Common/Graph.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Priority_Queue;
 
 namespace Common
 {
@@ -51,12 +52,13 @@ namespace Common
         public void BfsFrom(T start, Func<T, VisitPath, bool> visit)
         {
             Dictionary<T, T> visited = new Dictionary<T, T>();
-            HashSet<(T, T)> frontier = new HashSet<(T, T)>() { (start, start) };
+            Dictionary<T, T> frontier = new Dictionary<T, T>();
+            frontier.Add(start, start);
             int depth = 0;
             bool earlyExitRequested = false;
             while (frontier.Any() && !earlyExitRequested)
             {
-                HashSet<(T, T)> newFrontier = new HashSet<(T, T)>();
+                Dictionary<T, T> newFrontier = new Dictionary<T, T>();
                 foreach ((T node, T predecessor) in frontier)
                 {
                     visited.Add(node, predecessor);
@@ -64,9 +66,9 @@ namespace Common
                     earlyExitRequested = visit(node, path) || earlyExitRequested;
                     foreach (T neighbour in GetNeighbours(node))
                     {
-                        if (!visited.ContainsKey(neighbour))
+                        if (!visited.ContainsKey(neighbour) && !newFrontier.ContainsKey(neighbour))
                         {
-                            newFrontier.Add((neighbour, node));
+                            newFrontier.Add(neighbour, node);
                         }
                     }
                 }
@@ -185,7 +187,8 @@ namespace Common
     {
         private Func<T, IEnumerable<T>> GetEdges;
 
-        public GraphByFunction(Func<T, IEnumerable<T>> getEdges) {
+        public GraphByFunction(Func<T, IEnumerable<T>> getEdges)
+        {
             GetEdges = getEdges;
         }
 
@@ -200,4 +203,139 @@ namespace Common
         }
     }
 
+
+    public abstract class WeightedGraph<T>
+    {
+        public class VisitPath
+        {
+            private int Length;
+            private T Start;
+            private T End;
+            private Dictionary<T, T> Predecessors;
+
+            public VisitPath(T end, T start, int length, Dictionary<T, T> predecessors)
+            {
+                End = end;
+                Length = length;
+                Start = start;
+                Predecessors = predecessors;
+            }
+
+            public int GetLength()
+            {
+                return Length;
+            }
+
+            public IEnumerable<T> GetNodesOnPath()
+            {
+                T current = End;
+                Stack<T> reversePath = new Stack<T>();
+                while (!current.Equals(Start))
+                {
+                    T pred = Predecessors[current];
+                    reversePath.Push(current);
+                    current = pred;
+                }
+                while (reversePath.Any())
+                {
+                    yield return reversePath.Pop();
+                }
+                yield break;
+            }
+        }
+
+        public abstract IEnumerable<T> GetNodes();
+
+        public abstract IEnumerable<(T, int)> GetNeighbours(T node);
+
+        public void DijkstraFrom(T start, Func<T, VisitPath, bool> visit)
+        {
+            SimplePriorityQueue<T, int> queue = new SimplePriorityQueue<T, int>();
+            Dictionary<T, int> bestSoFar = new Dictionary<T, int>();
+            Dictionary<T, T> predecessor = new Dictionary<T, T>();
+
+            queue.Enqueue(start, 0);
+            bestSoFar.Add(start, 0);
+            predecessor.Add(start, start);
+            while (queue.Count != 0)
+            {
+                T node = queue.Dequeue();
+                if (visit(node, new VisitPath(node, start, bestSoFar[node], predecessor)))
+                {
+                    return;
+                }
+                foreach ((T node, int edgeWeight) neighbour in GetNeighbours(node))
+                {
+                    int potentialDist = bestSoFar[node] + neighbour.edgeWeight;
+                    if (potentialDist < bestSoFar.GetOrElse(neighbour.node, int.MaxValue))
+                    {
+                        bestSoFar.AddOrSet(neighbour.node, potentialDist);
+                        predecessor.AddOrSet(neighbour.node, node);
+                        if (queue.Contains(neighbour.node))
+                        {
+                            queue.UpdatePriority(neighbour.node, potentialDist);
+                        }
+                        else
+                        {
+                            queue.Enqueue(neighbour.node, potentialDist);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void DijkstraFrom(T start, Action<T, VisitPath> visit)
+        {
+            Func<T, VisitPath, bool> visitNoShortCircuit = (node, path) =>
+            {
+                visit(node, path);
+                return false;
+            };
+            DijkstraFrom(start, visitNoShortCircuit);
+        }
+
+        public VisitPath ShortestPathTo(T start, T end)
+        {
+            return ShortestPathTo(start, n => n.Equals(end));
+        }
+
+        public VisitPath ShortestPathTo(T start, Func<T, bool> predicate)
+        {
+            VisitPath pathToGoal = null;
+            DijkstraFrom(start, (node, path) =>
+            {
+                if (predicate(node))
+                {
+                    pathToGoal = path;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            });
+            return pathToGoal;
+        }
+    }
+
+    public class WeightedGraphByFunction<T> : WeightedGraph<T> where T : IEquatable<T>
+    {
+        private Func<T, IEnumerable<(T, int)>> GetEdges;
+
+        public WeightedGraphByFunction(Func<T, IEnumerable<(T, int)>> getEdges)
+        {
+            GetEdges = getEdges;
+        }
+
+        public override IEnumerable<(T, int)> GetNeighbours(T node)
+        {
+            return GetEdges(node);
+        }
+
+        public override IEnumerable<T> GetNodes()
+        {
+            throw new Exception("Not implemented");
+        }
+    }
 }
+
---
+++ Day18/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using Common;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day18
+{
+    class KeyCollection : IEquatable<KeyCollection>
+    {
+        IntPoint2D Pos;
+        int MovesSpent;
+        HashSet<char> CollectedKeys;
+
+        public KeyCollection(IntPoint2D pos)
+        {
+            Pos = pos;
+            MovesSpent = 0;
+            CollectedKeys = new HashSet<char>();
+        }
+
+        public KeyCollection(KeyCollection previous, IntPoint2D pos, int additionalMoves, char additionalKey)
+        {
+            Pos = pos;
+            MovesSpent = previous.MovesSpent + additionalMoves;
+            CollectedKeys = previous.CollectedKeys.ToHashSet();
+            CollectedKeys.Add(additionalKey);
+        }
+
+        public HashSet<char> GetKeys()
+        {
+            return CollectedKeys;
+        }
+
+        public IntPoint2D GetPos()
+        {
+            return Pos;
+        }
+
+        public int GetMoves()
+        {
+            return MovesSpent;
+        }
+
+        bool IEquatable<KeyCollection>.Equals(KeyCollection other)
+        {
+            return other.Pos == Pos
+                && other.MovesSpent == MovesSpent
+                && other.CollectedKeys.SetEquals(CollectedKeys);
+        }
+    }
+
+    class KeyFinder
+    {
+        KeyCollection Current;
+        KeyMaze Maze;
+        GraphByFunction<IntPoint2D> AvailableRoutes;
+
+        public KeyFinder(KeyCollection current, KeyMaze maze)
+        {
+            Current = current;
+            Maze = maze;
+            AvailableRoutes = new GraphByFunction<IntPoint2D>(p => Maze.CurrentMovesFrom(p, Current.GetKeys()));
+        }
+
+        public IEnumerable<(KeyCollection, int)> PossibleNextKeys()
+        {
+            List<(KeyCollection, int)> foundKeys = new List<(KeyCollection, int)>();
+            AvailableRoutes.BfsFrom(Current.GetPos(),
+                (pos, path) => {
+                    char newKey = Maze.GetTile(pos);
+                    if (Maze.IsKey(newKey) && !Current.GetKeys().Contains(newKey))
+                    {
+                        foundKeys.Add((
+                            new KeyCollection(Current, pos, path.GetLength(), newKey),
+                            path.GetLength()));
+                    }
+                });
+            return foundKeys;
+        }
+    }
+
+    class KeyMaze
+    {
+        Dictionary<IntPoint2D, char> Map;
+
+        static List<IntPoint2D> Directions = new List<IntPoint2D>()
+        {
+            new IntPoint2D(0, -1),
+            new IntPoint2D(0, 1),
+            new IntPoint2D(-1, 0),
+            new IntPoint2D(1, 0)
+        };
+
+        public KeyMaze(string fileName)
+        {
+            Map = SparseGrid.ReadFromFile(fileName);
+        }
+
+        public IntPoint2D GetStartPos()
+        {
+            return Map.First(kv => kv.Value == '@').Key;
+        }
+
+        public void Print()
+        {
+            SparseGrid.Print(Map, c => c);
+        }
+
+        public IEnumerable<IntPoint2D> CurrentMovesFrom(IntPoint2D pos, HashSet<char> collectedKeys)
+        {
+            return Directions.Select(d => d + pos).Where(p => CanEnter(p, collectedKeys));
+        }
+
+        private bool CanEnter(IntPoint2D p, HashSet<char> collectedKeys)
+        {
+            char tile = GetTile(p);
+            if (tile == '.')
+            {
+                return true;
+            }
+            if (tile == '#')
+            {
+                return false;
+            }
+            if (tile >= 'a' && tile <= 'z')
+            {
+                return true;
+            }
+            if (tile >= 'A' && tile <= 'Z')
+            {
+                return collectedKeys.Contains(tile.ToString().ToLower()[0]);
+            }
+            if (tile == '@')
+            {
+                return true;
+            }
+            throw new Exception($"Unknown tile type '{tile}'");
+        }
+
+        public char GetTile(IntPoint2D pos)
+        {
+            return Map.GetOrElse(pos, '#');
+        }
+
+        public bool IsKey(char tile)
+        {
+            return tile >= 'a' && tile <= 'z';
+        }
+
+        public IEnumerable<char> GetAllKeys()
+        {
+            return Map.Values.Where(IsKey);
+        }
+    }
+
+    class MazeSolver
+    {
+        WeightedGraphByFunction<KeyCollection> KeyCollectionOrders;
+        KeyMaze Maze;
+        KeyCollection Start;
+
+        public MazeSolver(string fileName)
+        {
+            Maze = new KeyMaze(fileName);
+            Start = new KeyCollection(Maze.GetStartPos());
+            KeyCollectionOrders = new WeightedGraphByFunction<KeyCollection>(NextKeysPossible);
+            Maze.Print();
+            throw new Exception("Exiting early");
+        }
+
+        IEnumerable<(KeyCollection, int)> NextKeysPossible(KeyCollection current)
+        {
+            return new KeyFinder(current, Maze).PossibleNextKeys();
+        }
+
+        public int Solve()
+        {
+            int bestLength = int.MaxValue;
+            KeyCollectionOrders.DijkstraFrom(Start,
+                (keys, earlier) =>
+                {
+                    if (keys.GetKeys().IsSupersetOf(Maze.GetAllKeys())) {
+                        if (keys.GetMoves() < bestLength)
+                        {
+                            bestLength = keys.GetMoves();
+                        }
+                    }
+                });
+            return bestLength;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MazeSolver solver = new MazeSolver("../../../input.txt");
+            Console.WriteLine(solver.Solve());
+        }
+    }
+}
