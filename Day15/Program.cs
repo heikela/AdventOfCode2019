a24628f9bdb0d4dd8ce945abd977e1ff9710b5ed
Mikko Heikelä <mikko.heikela@gmail.com>
2019-12-26 14:07:07 +0200

    Make day 15 more general and refactor it
    Explore the maze in part one in order of distance from
    start rather than distance from where the droid is.
    This is less efficient in terms of droid movement, but
    ensures that the first path to oxygen is indeed the shortest
    possible, and also ensures that it is found even if the maze
    would be open ended.
    
    Additionally refactor the code to allow more reuse between the
    first and second parts.

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -18,10 +18,12 @@ namespace Day15
     class Program
     {
         static Dictionary<IntPoint2D, Tile> Map = new Dictionary<IntPoint2D, Tile>();
-        static IntPoint2D Pos = new IntPoint2D();
-        static int ShortestDistance = int.MaxValue;
+        static IntPoint2D Start = new IntPoint2D();
+        static IntPoint2D Pos = Start;
         static int LongestDistance = int.MinValue;
         static IntPoint2D? OxygenPos = null;
+        static Graph<IntPoint2D> KnownMapGraph = new GraphByFunction<IntPoint2D>(KnownEdges);
+        static Graph<IntPoint2D> ExplorationGraph = new GraphByFunction<IntPoint2D>(ExplorationEdges);
 
         static List<IntPoint2D> Moves = new List<IntPoint2D>()
         {
@@ -90,111 +92,91 @@ namespace Day15
             }
         }
 
+        static IEnumerable<IntPoint2D> RouteToUnexploredNearestToStart()
+        {
+            Graph<IntPoint2D>.VisitPath startToUnexplored =
+                ExplorationGraph.ShortestPathTo(Start, pos => !Map.ContainsKey(pos));
+            if (startToUnexplored == null)
+            {
+                return new List<IntPoint2D>();
+            }
+            int startToUnexploredLen = startToUnexplored.GetLength();
+            IntPoint2D target = startToUnexplored.GetNodesOnPath().Last();
+            IntPoint2D lastKnownPoint = startToUnexploredLen > 1 ?
+                startToUnexplored.GetNodesOnPath().ElementAt(startToUnexploredLen - 2) :
+                Start;
+            return KnownMapGraph.ShortestPathTo(Pos, lastKnownPoint).GetNodesOnPath().Append(target);
+        }
+
+        static void NavigatePath(IEnumerable<IntPoint2D> path, IntCodeComputer droidControl)
+        {
+            foreach (IntPoint2D step in path)
+            {
+                Queue<BigInteger> input = new Queue<BigInteger>();
+                IntPoint2D direction = step - Pos;
+                input.Enqueue(Moves.IndexOf(direction) + 1);
+                var result = droidControl.RunIntCodeV11(input);
+                if (result.output.Count != 1)
+                {
+                    throw new Exception($"Expected one status code as output, got {result.output.Count}");
+                }
+                BigInteger status = result.output[0];
+                switch ((int)status)
+                {
+                    case 0:
+                        {
+                            Map.Add(step, Tile.Wall);
+                            break;
+                        }
+                    case 1:
+                        {
+                            Map.AddOrSet(step, Tile.Room);
+                            Pos = step;
+                            break;
+                        }
+                    case 2:
+                        {
+                            Map.AddOrSet(step, Tile.OxygenSystem);
+                            Pos = step;
+                            OxygenPos = Pos;
+                            break;
+                        }
+                    default:
+                        throw new Exception($"Unexpected status code {status}");
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             IntCodeComputer droidControl = new IntCodeComputer(File.ReadLines("../../../input.txt").First());
 
-            Map.Add(Pos, Tile.Start);
+            Map.Add(Start, Tile.Start);
 
-            List<IntPoint2D> pathToUnexplored = null;
-
-            Graph<IntPoint2D> knownMapGraph = new GraphByFunction<IntPoint2D>(KnownEdges);
-            Graph<IntPoint2D> explorationGraph = new GraphByFunction<IntPoint2D>(ExplorationEdges);
-            pathToUnexplored = explorationGraph.ShortestPathTo(Pos, pos => !Map.ContainsKey(pos)).GetNodesOnPath().ToList();
-
-            (bool running, List<BigInteger> output) result;
+            IEnumerable<IntPoint2D> pathToUnexplored = RouteToUnexploredNearestToStart();
 
             while (OxygenPos == null)
             {
-                while (pathToUnexplored.Any())
-                {
-                    Queue<BigInteger> input = new Queue<BigInteger>();
-                    IntPoint2D stepTarget = pathToUnexplored.First();
-                    pathToUnexplored.RemoveAt(0);
-                    IntPoint2D direction = stepTarget - Pos;
-                    input.Enqueue(Moves.IndexOf(direction) + 1);
-                    result = droidControl.RunIntCodeV11(input);
-                    if (result.output.Count != 1)
-                    {
-                        throw new Exception($"Expected one status code as output, got {result.output.Count}");
-                    }
-                    BigInteger status = result.output[0];
-                    switch ((int)status)
-                    {
-                        case 0:
-                            {
-                                Map.Add(stepTarget, Tile.Wall);
-                                break;
-                            }
-                        case 1:
-                            {
-                                Map.AddOrSet(stepTarget, Tile.Room);
-                                Pos = stepTarget;
-                                break;
-                            }
-                        case 2:
-                            {
-                                Map.AddOrSet(stepTarget, Tile.OxygenSystem);
-                                Pos = stepTarget;
-                                OxygenPos = stepTarget;
-                                break;
-                            }
-                        default:
-                            throw new Exception($"Unexpected status code {status}");
-                    }
-                }
-                SparseGrid.Print(Map, ShowTile);
-
-                pathToUnexplored = explorationGraph.ShortestPathTo(Pos, pos => !Map.ContainsKey(pos)).GetNodesOnPath().ToList();
+                NavigatePath(pathToUnexplored, droidControl);
+//                SparseGrid.Print(Map, ShowTile);
+                pathToUnexplored = RouteToUnexploredNearestToStart();
             }
 
-            Console.WriteLine($"Among the paths randomly explored, the shortest to oxygen is {knownMapGraph.ShortestPathTo(new IntPoint2D(), OxygenPos.Value).GetLength()} steps");
+            Console.WriteLine($"The shortest path to oxygen is {KnownMapGraph.ShortestPathTo(Start, OxygenPos.Value).GetLength()} steps");
 
-            while (pathToUnexplored != null)
+            while (pathToUnexplored.Any())
             {
-                while (pathToUnexplored.Any())
-                {
-                    Queue<BigInteger> input = new Queue<BigInteger>();
-                    IntPoint2D stepTarget = pathToUnexplored.First();
-                    pathToUnexplored.RemoveAt(0);
-                    IntPoint2D direction = stepTarget - Pos;
-                    input.Enqueue(Moves.IndexOf(direction) + 1);
-                    result = droidControl.RunIntCodeV11(input);
-                    if (result.output.Count != 1)
-                    {
-                        throw new Exception($"Expected one status code as output, got {result.output.Count}");
-                    }
-                    BigInteger status = result.output[0];
-                    switch ((int)status)
-                    {
-                        case 0:
-                            {
-                                Map.Add(stepTarget, Tile.Wall);
-                                break;
-                            }
-                        case 1:
-                            {
-                                Map.AddOrSet(stepTarget, Tile.Room);
-                                Pos = stepTarget;
-                                break;
-                            }
-                        case 2:
-                            {
-                                Map.AddOrSet(stepTarget, Tile.OxygenSystem);
-                                Pos = stepTarget;
-                                OxygenPos = stepTarget;
-                                break;
-                            }
-                        default:
-                            throw new Exception($"Unexpected status code {status}");
-                    }
-                }
-                SparseGrid.Print(Map, ShowTile);
-
-                pathToUnexplored = explorationGraph.ShortestPathTo(Pos, pos => !Map.ContainsKey(pos))?.GetNodesOnPath()?.ToList();
+                NavigatePath(pathToUnexplored, droidControl);
+//                SparseGrid.Print(Map, ShowTile);
+                pathToUnexplored = RouteToUnexploredNearestToStart();
             }
 
-            knownMapGraph.BfsFrom(OxygenPos.Value, LongestDistanceFromOxygen);
+            /* The task in part two reveals that the maze is not infinite. This means
+             * that an exhaustive search would be a possible way to solve part 1 as well,
+             * but I have gone for an approach that would solve part 1 even
+             * in an infinite maze */
+
+            KnownMapGraph.BfsFrom(OxygenPos.Value, LongestDistanceFromOxygen);
             Console.WriteLine($"After exploring the map, the longest distance to oxygen is {LongestDistance} steps");
         }
     }
