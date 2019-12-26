f58dfe098c47688e3c91c19834936c1657f08e1c
Mikko Heikelä <mikko.heikela@gmail.com>
2019-12-26 11:19:10 +0200

    Extract a generic period detector from Day 12

---
+++ Common/PeriodDetector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public class PeriodDetector<T> where T : IEquatable<T>
+    {
+        Dictionary<T, int> SeenStates;
+        int Time;
+        int? Period;
+
+        public PeriodDetector()
+        {
+            SeenStates = new Dictionary<T, int>();
+            Time = 0;
+            Period = null;
+        }
+
+        public bool PeriodFound()
+        {
+            return Period.HasValue;
+        }
+
+        public bool DetectPeriod(T newValue)
+        {
+            if (SeenStates.ContainsKey(newValue))
+            {
+                if (Period.HasValue)
+                {
+                    int potentialPeriod = Time - SeenStates[newValue];
+                    if (potentialPeriod % Period.Value != 0)
+                    {
+                        throw new Exception("Disagreement on period value");
+                    }
+                    Time++;
+                    return true;
+                }
+                else
+                {
+                    Period = Time - SeenStates[newValue];
+                    Time++;
+                    return true;
+                }
+            }
+            else
+            {
+                SeenStates[newValue] = Time++;
+                return false;
+            }
+        }
+
+        public int GetPeriod()
+        {
+            if (!Period.HasValue)
+            {
+                throw new Exception("Period not detected yet");
+            }
+            return Period.Value;
+        }
+    }
+}
--- Day12/Program.cs
+++ Day12/Program.cs
@@ -50,64 +50,17 @@ namespace Day12
             };
             List<IntPoint3D> vel = pos.Select(x => new IntPoint3D()).ToList();
             int time = 0;
-            //int maxT = 10;
-            bool Xrepeated = false;
-            bool Yrepeated = false;
-            bool Zrepeated = false;
-            int Xcycle = 0;
-            int Ycycle = 0;
-            int Zcycle = 0;
-            Dictionary<(int, int, int, int, int, int, int, int), int> timeByX = new Dictionary<(int, int, int, int, int, int, int, int), int>();
-            Dictionary<(int, int, int, int, int, int, int, int), int> timeByY = new Dictionary<(int, int, int, int, int, int, int, int), int>();
-            Dictionary<(int, int, int, int, int, int, int, int), int> timeByZ = new Dictionary<(int, int, int, int, int, int, int, int), int>();
-            while (!Xrepeated || !Yrepeated || !Zrepeated)
+            PeriodDetector<(int, int, int, int, int, int, int, int)> XPeriod = new PeriodDetector<(int, int, int, int, int, int, int, int)>();
+            PeriodDetector<(int, int, int, int, int, int, int, int)> YPeriod = new PeriodDetector<(int, int, int, int, int, int, int, int)>();
+            PeriodDetector<(int, int, int, int, int, int, int, int)> ZPeriod = new PeriodDetector<(int, int, int, int, int, int, int, int)>();
+            while (!XPeriod.PeriodFound() || !YPeriod.PeriodFound() || !ZPeriod.PeriodFound())
             {
-                if (!Xrepeated) {
-                    var xState = (pos[0].X, pos[1].X, pos[2].X, pos[3].X, vel[0].X, vel[1].X, vel[2].X, vel[3].X);
-                    if (timeByX.ContainsKey(xState))
-                    {
-                        int prevTime = timeByX[xState];
-                        int delta = time - prevTime;
-                        Console.WriteLine($"Judging by coordinate X, at Time {time} we have a state similar to time {prevTime}, {delta} steps earlier.");
-                        Xrepeated = true;
-                        Xcycle = delta;
-                    }
-                    else
-                    {
-                        timeByX.Add(xState, time);
-                    }
-                }
-                if (!Yrepeated)
-                {
-                    var yState = (pos[0].Y, pos[1].Y, pos[2].Y, pos[3].Y, vel[0].Y, vel[1].Y, vel[2].Y, vel[3].Y);
-                    if (timeByY.ContainsKey(yState))
-                    {
-                        int prevTime = timeByY[yState];
-                        int delta = time - prevTime;
-                        Console.WriteLine($"Judging by coordinate Y, at Time {time} we have a state similar to time {prevTime}, {delta} steps earlier.");
-                        Yrepeated = true;
-                        Ycycle = delta;
-                    }
-                    else
-                    {
-                        timeByY.Add(yState, time);
-                    }
-                }
-                if (!Zrepeated) {
-                    var zState = (pos[0].Z, pos[1].Z, pos[2].Z, pos[3].Z, vel[0].Z, vel[1].Z, vel[2].Z, vel[3].Z);
-                    if (timeByZ.ContainsKey(zState))
-                    {
-                        int prevTime = timeByZ[zState];
-                        int delta = time - prevTime;
-                        Console.WriteLine($"Judging by coordinate Z, at Time {time} we have a state similar to time {prevTime}, {delta} steps earlier.");
-                        Zrepeated = true;
-                        Zcycle = delta;
-                    }
-                    else
-                    {
-                        timeByZ.Add(zState, time);
-                    }
-                }
+                var xState = (pos[0].X, pos[1].X, pos[2].X, pos[3].X, vel[0].X, vel[1].X, vel[2].X, vel[3].X);
+                XPeriod.DetectPeriod(xState);
+                var yState = (pos[0].Y, pos[1].Y, pos[2].Y, pos[3].Y, vel[0].Y, vel[1].Y, vel[2].Y, vel[3].Y);
+                YPeriod.DetectPeriod(yState);
+                var zState = (pos[0].Z, pos[1].Z, pos[2].Z, pos[3].Z, vel[0].Z, vel[1].Z, vel[2].Z, vel[3].Z);
+                ZPeriod.DetectPeriod(zState);
                 Accelerate(pos, vel);
                 Move(pos, vel);
                 time++;
@@ -116,7 +69,9 @@ namespace Day12
                     Console.WriteLine($"Total Energy {TotalEnergy(pos, vel)}");
                 }
             }
-            Console.WriteLine($"The moons follow a cycle of {MathUtils.LCD(Xcycle, MathUtils.LCD(Ycycle, Zcycle))}");
+            long cycle = MathUtils.LCD(XPeriod.GetPeriod(),
+                MathUtils.LCD(YPeriod.GetPeriod(), ZPeriod.GetPeriod()));
+            Console.WriteLine($"The moons follow a cycle of {cycle} steps");
         }
     }
 }
